ac48837d7ac396bc0db7aa590d70b19984e793d8
Mintify <149532589+eventwiki@users.noreply.github.com>
2024-02-09 20:50:51 +0700

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -2,13 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
- name: string
-hello.ts
+  name: string
 }
 
 export default function handler(
- req: NextApiRequest,
- res: NextApiResponse<Data>
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
 ) {
- res.status(200).json({ name: 'eventwiki' })
+  res.status(200).json({ name: 'John Doe' })
 }
